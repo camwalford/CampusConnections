6c48e39759a9a432b88990c7c2c79dcbf8b267c6
Keagan Purtell <keaganpurtell@Hotmail.com>
2022-10-28 13:46:23 -0700

    firebase authentication implemented, not implemented on all pages yet

---
+++ scripts/firebaseAPI_TEAM26.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDXvFv2OgN_2Z05cUrZe_iN8dNFEPKGpc8",
+    authDomain: "campus-connections-cd94f.firebaseapp.com",
+    projectId: "campus-connections-cd94f",
+    storageBucket: "campus-connections-cd94f.appspot.com",
+    messagingSenderId: "1059368902891",
+    appId: "1:1059368902891:web:fc578d5f2ba6a0005b9eae"
+  };
+  
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
