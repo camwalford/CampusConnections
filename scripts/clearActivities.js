01ffaf2f8f998b0cb172012108ccbb83330e259a
Keagan Purtell <keaganpurtell@Hotmail.com>
2022-11-03 13:00:00 -0700

    added a callable function to delete everything in the activities collection

--- scripts/clearActivities.js
+++ scripts/clearActivities.js
@@ -1,4 +1,20 @@
 function clearActivities() {
     var activitiesRef = db.collection("activities");
-    activitiesRef.forEach(activitiesRef.get().delete());
+    activitiesRef.get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            //console.log(doc.id);
+            activitiesRef.doc(doc.id).delete();
+        })
+    })
+}
+
+
+function clearActivities() {
+    var activitiesRef = db.collection("activities");
+    activitiesRef.get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            //console.log(doc.id);
+            activitiesRef.doc(doc.id).delete();
+        })
+    })
 }
