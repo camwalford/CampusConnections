6d15020cc9d12ce5bbc17442dc812a3929204cf4
Cameron Walford <cwalford2012@gmail.com>
2022-11-09 21:17:38 -0800

    Chat.js now saves messages to correct collection with proper fields

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,19 +1,23 @@
 
+//Invokes a click listener for the message-submit button
 document.getElementById("message-submit").addEventListener("click", function(e){
     firebase.auth().onAuthStateChanged(async(user) => {
-
         
+        //Checks if there is a current user logged in
         if(user){
             var uid = user.uid;
             console.log("uid of current user is " + uid);
-            //const currentGroupRef = await getGroup(uid);
+           
+            //Synchronously retrieves current user's group then executes function inside
             db.collection("users").doc(uid).get().then(function(doc) {
                 var currentGroupRef = doc.data().currentGroup;
-                console.log(doc.data().currentGroup);
                 console.log("currentGroupRef is equal to " + currentGroupRef);
-                console.log("currentGroupRef is" + currentGroupRef);
+                
+                //Checks if currentGroup is undefined or null
                 if(currentGroupRef != null || undefined){
                     console.log("the current users group is " + currentGroupRef);
+
+                    //passes in the users current group to the sendMessage function
                     sendMessage(currentGroupRef);
                 }else{
                     console.log("the current user is not in a group.");
@@ -24,79 +28,35 @@ document.getElementById("message-submit").addEventListener("click", function(e){
             console.log("The current user is not logged in :/");
         }
 
+        function sendMessage(groupRef){
+           
+            console.log("the sendMessage function was invoked by: " + user.displayName); 
+
+            //Retrieves the message and timestamp
+            var messageInput = document.getElementById("message-input");
+            var message = messageInput.value;
+            var timestamp = Date.now();
+            console.log("message value: " + message);
+            
+            // clears the message input box
+            messageInput.value = "";
+        
+            //auto scroll to bottom
+            // document
+            // .getElementById("messages")
+            // .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+            //if(typeof message === 'string' && message.trim() !== ''){
+            //Adds message document to activity's chat collection with data.
+                db.collection("activities").doc(groupRef).collection("chats").add({
+                    username: user.displayName,
+                    message: message,
+                    timesent: timestamp
+                });
+        };
+
+
     });
-
-    // async function getGroup(uid){
-
-    //     var groupRef;
-    //     db.collection("users").doc(uid).get().then(function(doc) {
-    //         groupRef = doc.data['currentGroup'];
-    //         console.log("getGroup groupRef is equal to " + groupRef);
-    //     });
-
-    // };
-
-        //     const currentGroup = db.collection("users").doc(uid).get("currentGroup").then()
-
-        //     if(currentGroup != null){
-        //         console.log("the current users group is " + currentGroup);
-        //         sendMessage(currentGroup);
-        //     }else{
-        //         console.log("the current user is not in a group.");
-        //     }
-        // }else{
-        //     console.log("The current user is not logged in :/");
-        //}
-    function sendMessage(group){
-        console.log("the sendMessage function was run" + user.displayName)
-        
-    
-        var timestamp = Date.now();
-        var messageSubmit = document.getElementById("message-submit");
-        var message = messageSubmit.value;
-        
-        // clears the message input box
-        messageSubmit.value = "";
-    
-        //auto scroll to bottom
-        // document
-        // .getElementById("messages")
-        // .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-    
-        // create db collection and send in the data
-        db.collection("activities").doc(group).collection("chats").add({
-            username: user.displayName,
-            message: message,
-            time: timestamp
-        });
-    };
 });
-
-
-
-// function sendMessage(group){
-//     console.log("the sendMessage function was run" + user.displayName)
-    
-
-//     var timestamp = Date.now();
-//     var messageSubmit = document.getElementById("message-submit");
-//     var message = messageSubmit.value;
-    
-//     // clears the message input box
-//     messageSubmit.value = "";
-
-//     //auto scroll to bottom
-//     // document
-//     // .getElementById("messages")
-//     // .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-
-//     // create db collection and send in the data
-//     db.collection("activities").doc(group).collection("chats").add({
-//         username: user.displayName,
-//         message: message,
-//         time: timestamp
-//     });
-// };
 
 // const fetchChat = db.collection("activities").doc(currentGroupRef).collection("chats");
 
