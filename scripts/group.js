769104e176ea5809898024122b6a9be034123408
Keagan Purtell <keaganpurtell@hotmail.com>
2022-11-09 21:29:28 -0800

    worked on group.js and group.html for a while but completed nothing, reverted everything to functional though added a WIPgroup.html

--- scripts/group.js
+++ scripts/group.js
@@ -1,34 +1,125 @@
+// function displayCards(collection) {
+//     let cardTemplate = document.getElementById("activityTemplate");
+
+//             console.log("uid of current user is " + currentUserID);
+//             if(currentUserID) {
+
+//             var currentUserRef = db.collection("users").doc(currentUserID)
+//             .then(() => {
+//                 var currentGroup = currentUserRef.get("currentGroup")
+//             })
+//             .then(() => {
+//                 console.log(currentGroup);
+//                 //needs activity with the same name as user current group
+//                 activt = db.collection("activities").get(currentGroup);
+//                     var title = activt.title;        // get value of the "name" key
+//                     var activityType = activt.activityType;   // get value of the "details" key
+//                     var building = activt.building;
+//                     var participants = activt.participants;
+//                     var description = activt.description;
+
+//                     var hikeID = activt.code;    //get unique ID to each hike to be used for fetching right image
+//                     let newcard = cardTemplate.content.cloneNode(true);
+
+//                     //update title and text and image
+//                     newcard.querySelector('.card-title').innerHTML = title;
+//                     newcard.querySelector('.card-text').innerHTML = "type: " + activityType + "<br/>building: " + building + "<br/>participants: " + participants + "<br/>description: " + description;
+//                     // newcard.querySelector('.card-image').src = `./images/${hikeID}.jpg`; //Example: NV01.jpg
+
+//                     //give unique ids to all elements for future use
+//                     // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+//                     // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+//                     // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+//                     //attach to gallery
+//                     document.getElementById(collection + "-go-here").appendChild(newcard);
+//             })
+//         }
+
+// }
+
 function displayCards(collection) {
-    let cardTemplate = document.getElementById("activityTemplate");
+  let cardTemplate = document.getElementById("activityTemplate");
+//   console.log("test " + (currentUserID));
 
-    var currentUserRef = db.collection("users").doc(currentUserID);
-    var currentGroup = currentUserRef.get("currentGroup");
-    console.log(currentGroup);
-//needs activity with the same name as user current group
-    activt = db.collection("activities").get(currentGroup);
-                var title = activt.data().title;        // get value of the "name" key
-                var activityType = activt.data().activityType;   // get value of the "details" key
-                var building = activt.data().building;
-                var participants = activt.data().participants;
-                var description = activt.data().description;
+  var currentGroupRef = db.collection("users").doc(currentUserID).get("currentGroup")
+    .then((snip) => {
+      db.collection(collection).get()
+        .then((snap) => {
+          snap.forEach((doc) => {
+            //iterate thru each doc
+            console.log(doc.id + " " + snip);
 
-								
-                var hikeID = activt.data().code;    //get unique ID to each hike to be used for fetching right image
-                let newcard = cardTemplate.content.cloneNode(true);
+            if (currentGroupRef == doc.id) {
+              console.log(doc.id + "" + currentUserRef.currentGroup);
+              var title = doc.data().title; // get value of the "name" key
+              var activityType = doc.data().activityType; // get value of the "details" key
+              var activityID = doc.id; //gets the unique id for the activity
+              var building = doc.data().building;
+              var participants = doc.data().participants;
+              var description = doc.data().description;
 
-                //update title and text and image
-                newcard.querySelector('.card-title').innerHTML = title;
-                newcard.querySelector('.card-text').innerHTML = "type: " + activityType + "<br/>building: " + building + "<br/>participants: " + participants + "<br/>description: " + description;
-                // newcard.querySelector('.card-image').src = `./images/${hikeID}.jpg`; //Example: NV01.jpg
+               let newcard = cardTemplate.content.cloneNode(true);
 
-                //give unique ids to all elements for future use
-                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
-                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
-                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+              //update title and text and image
+              newcard.querySelector(".card-title").innerHTML = title;
+              newcard.querySelector(".card-text").innerHTML =
+                "type: " +
+                activityType +
+                "<br/>building: " +
+                building +
+                "<br/>participants: " +
+                participants +
+                "<br/>description: " +
+                description;
+              // newcard.querySelector('.card-image').src = `./images/${hikeID}.jpg`; //Example: NV01.jpg
 
-                //attach to gallery
-                document.getElementById(collection + "-go-here").appendChild(newcard);
-                //i++;   //if you want to use commented out section
+              //give unique ids to all elements for future use
+              newcard.querySelector("a").onclick = () => joinGroup(activityID);
+
+              //attach to gallery
+              document
+                .getElementById(collection + "-go-here")
+                .appendChild(newcard);
+            }
+          });
+        });
+    });
 }
 
-displayCards("activities");
+function displayGroup() {
+  let cardTemplate = document.getElementById("activityTemplate");
+  console.log(currentUserID); //user doesn't exist yet which is super lame
+  var currentGroupRef = db.collection("users").doc(currentUserID).get("currentGroup")
+  .then((snapshot) => {
+      var currentActivity = db.collection("activities").get(snapshot)
+        .then((snap) => {
+          console.log(snapshot);
+
+          var title = snap.title; 
+          var activityType = snap.activityType; 
+          var building = snap.building;
+          var participants = snap.participants;
+          var description = snap.description;
+
+          let newcard = cardTemplate.content.cloneNode(true);
+
+          //update title and text and image
+          newcard.querySelector(".card-title").innerHTML = title;
+          newcard.querySelector(".card-text").innerHTML =
+            "type: " +
+            activityType +
+            "<br/>building: " +
+            building +
+            "<br/>participants: " +
+            participants +
+            "<br/>description: " +
+            description;
+
+          document.getElementById("activities-go-here").appendChild(newcard);
+        });
+    });
+}
+
+//displayCards("activities");
+displayGroup();
