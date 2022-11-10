46e1c66d937088c0ba381aa28d7c37960fb954be
Keagan Purtell <keaganpurtell@hotmail.com>
2022-11-09 18:37:42 -0800

    joining groups now accurately updates the currentGroup for the user on firestore

--- scripts/activities.js
+++ scripts/activities.js
@@ -7,12 +7,13 @@ function displayCards(collection) {
             snap.forEach(doc => { //iterate thru each doc
                 var title = doc.data().title;        // get value of the "name" key
                 var activityType = doc.data().activityType;   // get value of the "details" key
+                var activityID = doc.id; //gets the unique id for the activity
                 var building = doc.data().building;
                 var participants = doc.data().participants;
                 var description = doc.data().description;
 
 								
-                var hikeID = doc.data().code;    //get unique ID to each hike to be used for fetching right image
+                //var hikeID = doc.data().code;    //get unique ID to each hike to be used for fetching right image
                 let newcard = cardTemplate.content.cloneNode(true);
 
                 //update title and text and image
@@ -21,15 +22,25 @@ function displayCards(collection) {
                 // newcard.querySelector('.card-image').src = `./images/${hikeID}.jpg`; //Example: NV01.jpg
 
                 //give unique ids to all elements for future use
-                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
-                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
-                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+                newcard.querySelector('a').onclick = () => joinGroup(activityID);
 
                 //attach to gallery
                 document.getElementById(collection + "-go-here").appendChild(newcard);
-                //i++;   //if you want to use commented out section
             })
         })
+}
+
+function joinGroup(id){
+    console.log("uid of current user is" + currentUserID);
+    console.log("uid of activity is " + id);
+    var currentUserRef = db.collection("users").doc(currentUserID);
+    currentUserRef.update({
+        currentGroup: id,
+    }).then(() => {
+        window.open("group.html", "_self");
+    });
+
+
 }
 
 displayCards("activities");
