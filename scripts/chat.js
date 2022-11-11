896ff240d1180bbc0a69eb022f98230a1f70fcd9
Cameron Walford <cwalford2012@gmail.com>
2022-11-11 13:53:35 -0800

    Messages  and sender now appear on page load

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,5 +1,6 @@
-
+displayCurrentMessages()
 //Invokes a click listener for the message-submit button
+
 document.getElementById("message-submit").addEventListener("click", function(e){
     firebase.auth().onAuthStateChanged(async(user) => {
         //Checks if there is a current user logged in
@@ -10,11 +11,11 @@ document.getElementById("message-submit").addEventListener("click", function(e){
             //Synchronously retrieves current user's group then executes function inside
             db.collection("users").doc(uid).get().then(function(doc) {
                 var currentGroupRef = doc.data().currentGroup;
-                console.log("currentGroupRef is equal to " + currentGroupRef);
+                //console.log("currentGroupRef is equal to " + currentGroupRef);
                 
                 //Checks if currentGroup is undefined or null
                 if(currentGroupRef != null || undefined){
-                    console.log("the current users group is " + currentGroupRef);
+                    console.log("the current user's group is " + currentGroupRef);
 
                     //passes in the users current group to the sendMessage function
                     sendMessage(currentGroupRef);
@@ -52,6 +53,7 @@ document.getElementById("message-submit").addEventListener("click", function(e){
                     message: message,
                     timesent: timestamp
                 });
+                console.log("Message was sent")
 
             }else{
                 alert("🤡🤡🤡 no empty messages 🤡🤡🤡")
@@ -62,3 +64,71 @@ document.getElementById("message-submit").addEventListener("click", function(e){
     });
 });
 
+function displayCurrentMessages(){
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            let messageList = document.getElementById("messages");
+            let currentUserID = user.uid;
+            let str;
+            console.log("displaymessages " + currentUserID);
+            
+            var currentGroupRef = db
+            .collection("users")
+            .doc(currentUserID)
+            .get()
+            .then((doc) => {
+                console.log(doc.data().currentGroup);
+                
+                var currentChats = db
+                    .collection("activities")
+                    .doc(doc.data().currentGroup)
+                    .collection("chats")
+                    .get()
+                    .then((snapshot) => {
+                        console.log(snapshot);
+
+                        //Each message in the database is added as a list item.
+                        snapshot.forEach((doc) =>{
+                            console.log(doc.id, " ", doc.data().message);
+                            // str = ("<li><span class=\"sent-by\">" 
+                            //     + doc.data().username + "</span>" 
+                            //     + "<span class=\"sent-message\">" 
+                            //     + doc.data().message + "</span></li>"); 
+                            let li = document.createElement("li")
+                            
+                            li.innerHTML = ("<li><span class=\"sent-by\">" 
+                                + doc.data().username + "</span>" 
+                                + "<span class=\"sent-message\">" 
+                                + doc.data().message + "</span></li>");
+                            
+                            messageList.appendChild(li);
+
+
+                            //let nameOfSender = document.createTextNode(doc.data().username); 
+                            
+                            //span = do                          
+                        });
+                        console.log(str);
+
+                        
+                        // snapshot.docChanges().forEach(function(change){
+                        //     if(change.type === "added"){
+                        //         console.log("New Message ", change.doc.data());
+                                
+                        //     }
+                        //     if(change.type === "removed"){
+                        //         console.log("Message Removed: ", change.doc.data())
+                        //     }
+
+                        
+
+
+
+                    });
+            });
+
+            
+
+        }
+    });
+}
