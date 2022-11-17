8aff380ccbb62954af5a8b8b68383e6f4b80a8ae
Keagan Purtell <keaganpurtell@hotmail.com>
2022-11-16 21:12:17 -0800

    profile.html now pulls from the users profile, is not editable yet

---
+++ scripts/profile.js
@@ -0,0 +1,24 @@
+function displayProfile() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log(user.uid);
+        console.log(user.name);
+        console.log(user.email);
+        document.getElementById("username").innerHTML = "user: " + user.name;
+        document.getElementById("email").innerHTML = "email: " + user.email;
+        document.getElementById("users-program").innerHTML = "program: " + user.program;
+        document.getElementById("schoolyear").innerHTML = "year: " + user.year;
+
+        document.getElementsByID("edit").onclick = () => edit(activityID);
+      } else {
+        console.log("no one is logged in");
+      }
+    });
+  }
+
+  function edit() {
+    console.log("edit");
+  }
+  
+  displayProfile();
+  
