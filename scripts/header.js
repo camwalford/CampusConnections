b7dd27ed1334f8b91b9a7fa6b8ed100c1752ab22
Keagan Purtell <keaganpurtell@hotmail.com>
2022-11-22 21:41:06 -0800

    replaced settings button with pfp

---
+++ scripts/header.js
@@ -0,0 +1,23 @@
+function displayProfile() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        //go to the correct user document by referencing to the user uid
+        currentUser = db.collection("users").doc(user.uid);
+        //get the document for current user.
+        currentUser.get().then((userDoc) => {
+          //get the data fields of the user
+          let picUrl = userDoc.data().profilePic; 
+
+          if (picUrl != null) {
+            //console.log(picUrl);
+            // use this line if "mypicdiv" is a "div"
+            //$("#mypicdiv").append("<img src='" + picUrl + "'>")
+            $("#mypic-goes-here").attr("src", picUrl);
+          } else console.log("picURL is null");
+        });
+      } else {
+        console.log("no one is logged in");
+      }
+    });
+  }
+  displayProfile();
