f2848a50c6c63757ea06de3af2d586c39da71cca
Keagan Purtell <keaganpurtell@Hotmail.com>
2022-11-23 10:29:04 -0800

    fixed error where time would show up as 5:2 instead of 5:20

--- scripts/group.js
+++ scripts/group.js
@@ -14,14 +14,13 @@ function displayGroup() {
           console.log(doc.data().currentGroup);
 
           //Checks if user is in a group and displays the group information and chat if it's true.
-          if(doc.data().currentGroup !== "none"){
+          if (doc.data().currentGroup !== "none") {
             var currentGroup = db
               .collection("groups")
               .doc(doc.data().currentGroup)
               .get()
               .then((snap) => {
                 console.log(snap.data());
-                
 
                 var title = snap.data().title;
                 var groupType = snap.data().groupType;
@@ -34,21 +33,51 @@ function displayGroup() {
                 var description = snap.data().description;
                 var startTime;
                 var endTime = snap.data().endtime;
-                console.log("id is "+ groupID);
+                console.log("id is " + groupID);
 
-                if(startTime.toDate().getHours() > 12) {
-                  startTime = startTime.toDate().getHours()-12 + ":" + startTime.toDate().getMinutes()+"pm";
+                //this code fixes an error where if the time should say 5:02 it would say 5:2
+                let startMinutes;
+                if (startTime.toDate().getMinutes() < 10) {
+                  startMinutes = "0" + startTime.toDate().getMinutes();
                 } else {
-                  startTime = startTime.toDate().getHours() + ":" + startTime.toDate().getMinutes()+"am";
+                  startMinutes = startTime.toDate().getMinutes();
+                }
+                let endMinutes;
+                if (endTime.toDate().getMinutes() < 10) {
+                  endMinutes = "0" + endTime.toDate().getMinutes();
+                } else {
+                  endMinutes = endTime.toDate().getMinutes();
                 }
 
-                if(endTime.toDate().getHours() > 12) {
-                  endTime = endTime.toDate().getHours()-12 + ":" + endTime.toDate().getMinutes()+"pm";
+                if (startTime.toDate().getHours() > 12) {
+                  startTime =
+                    startTime.toDate().getHours() -
+                    12 +
+                    ":" +
+                    startMinutes +
+                    "pm";
                 } else {
-                  endTime = endTime.toDate().getHours() + ":" + endTime.toDate().getMinutes()+"am";
+                  startTime =
+                    startTime.toDate().getHours() +
+                    ":" +
+                    startMinutes +
+                    "am";
                 }
 
-                
+                if (endTime.toDate().getHours() > 12) {
+                  endTime =
+                    endTime.toDate().getHours() -
+                    12 +
+                    ":" +
+                    endMinutes +
+                    "pm";
+                } else {
+                  endTime =
+                    endTime.toDate().getHours() +
+                    ":" +
+                    endMinutes +
+                    "am";
+                }
 
                 //endTime = endTime.toDate().getHours() + ":" + endTime.toDate().getMinutes();
 
@@ -56,35 +85,38 @@ function displayGroup() {
 
                 //update title and text and image
                 newGroup.getElementById("group-title").innerHTML = title;
-                newGroup.getElementById("group-type").innerHTML =
-                groupType;
+                newGroup.getElementById("group-type").innerHTML = groupType;
                 newGroup.getElementById("group-building").innerHTML =
-                "<span id=\"pin\" class=\"material-symbols-outlined\">location_on</span>"+ building;
+                  '<span id="pin" class="material-symbols-outlined">location_on</span>' +
+                  building;
                 newGroup.getElementById("group-time").innerHTML =
-                "<span id=\"clock\" class=\"material-symbols-outlined\">schedule</span>" + startTime + " - " + endTime;
+                  '<span id="clock" class="material-symbols-outlined">schedule</span>' +
+                  startTime +
+                  " - " +
+                  endTime;
                 newGroup.getElementById("group-participants").innerHTML =
-                "<span id=\"person\" class=\"material-icons\">person</span>" + currentParticipants + "/" +  maxParticipants;
+                  '<span id="person" class="material-icons">person</span>' +
+                  currentParticipants +
+                  "/" +
+                  maxParticipants;
                 newGroup.getElementById("group-description").innerHTML =
-                "Description: " +  description;
-                
-              
+                  "Description: " + description;
 
-                document.querySelector("#leave").onclick = () => leaveGroup(groupID);
+                document.querySelector("#leave").onclick = () =>
+                  leaveGroup(groupID);
 
-                document
-                  .getElementById("groups-go-here")
-                  .appendChild(newGroup);
-                
-                var chat = document.getElementById('chat');
-                chat.style.display="block";
-                
+                document.getElementById("groups-go-here").appendChild(newGroup);
+
+                var chat = document.getElementById("chat");
+                chat.style.display = "block";
               });
-          //If user is not in a group, displays message and link to groupList page
-          }else{
-            document.getElementById("currentGroupContainer").innerHTML = "<div onclick=\"backToMap()\" id=\"exitButton\" class=\" newButton\">"
-            + "<span id=\"exitButton\" class=\"material-symbols-outlined\">close</span>"
-            + "</div><div id=\"noGroup\"><p>You are not currently in a group.</p>"
-            + "<a id=\"noGroupLink\" href=\"./groupsList.html\">Find a new group here!</a></div>"
+            //If user is not in a group, displays message and link to groupList page
+          } else {
+            document.getElementById("currentGroupContainer").innerHTML =
+              '<div onclick="backToMap()" id="exitButton" class=" newButton">' +
+              '<span id="exitButton" class="material-symbols-outlined">close</span>' +
+              '</div><div id="noGroup"><p>You are not currently in a group.</p>' +
+              '<a id="noGroupLink" href="./groupsList.html">Find a new group here!</a></div>';
           }
         });
 
@@ -112,21 +144,18 @@ function leaveGroup(id) {
 
   GroupRef.update({
     currentParticipants: firebase.firestore.FieldValue.increment(-1),
-  })
-  .then(() => {
+  }).then(() => {
     window.open("groupsList.html", "_self");
   });
 }
 
-
 displayGroup();
 
-
-var modal = document.getElementById('id01');
+var modal = document.getElementById("id01");
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
