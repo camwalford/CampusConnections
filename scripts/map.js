fea613959a512e344532001bd9d66565be26dc60
Cameron Walford <cwalford2012@gmail.com>
2022-11-26 23:06:39 -0800

    Added hover effect for each building area. Likely need to adjust areas.

---
+++ scripts/map.js
@@ -0,0 +1,25 @@
+let areas = document.querySelectorAll("area");
+let buildings = document.querySelectorAll(".buildings");
+let map = document.getElementById("map");
+let wrapper = document.getElementById("wrapper");
+
+function mouseAreas(){
+    for(let i = 0; i < areas.length; i++){
+        let id = areas[i].id;
+        areas[i].addEventListener("mouseover", function(){
+            let buildingID = id + 1;
+            buildingMap = document.getElementById(buildingID);
+            buildingMap.classList.add("zhigh");
+            wrapper.classList.add("zmiddle");
+        });
+        areas[i].addEventListener("mouseout", function(){
+            let buildingID = id + 1;
+            buildingMap = document.getElementById(buildingID);
+            buildingMap.classList.remove("zhigh");
+            wrapper.classList.remove("zmiddle");
+        });
+
+    }
+}
+
+mouseAreas();
